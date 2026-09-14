8d4991aca6baeb078bad2588cbd3e11849baa858
Jack <jack0browns0@gmail.com>
2026-09-14 08:21:26 +0300

    Izpildits 1.4

--- Hello.cs
+++ Hello.cs
@@ -3,14 +3,21 @@ using System;
 
 public partial class Hello : Node
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		GD.Print("Sveiks no C#!");
-	}
+    private int counter = 0;
+    private string playerName = "Ralfs";
+	private Vector2 spawnPosition = new Vector2(640, 360);
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    public override void _Ready()
+    {
+        GD.Print(counter = 0);
+        GD.Print(playerName = "Ralfs");
+        GD.Print(spawnPosition = new Vector2(640, 360));
+        SayHello();
+    }
+
+    public void SayHello()
+    {
+        counter++;
+        GD.Print("Sveiciens Nr. ", counter, " spēlētājam ", playerName);
+    }
 }
